23ff1284121c2ef7837089453a6e116f14fe256f
Damian Ciasnocha <daneek80@gmail.com>
2019-11-11 21:42:19 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let button = document.getElementById("enter");
+let input = document.getElementById("userinput");
+let ul = document.querySelector("ul");
+
+button.addEventListener("click", addListAfterClick);
